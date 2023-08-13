9564d771b45167015a4967948c63064cb096a99b
Андрей Брижак <andrewbrizhak@gmail.com>
2023-08-13 11:45:43 +0800

    Add Dictionary Rooms availability

---
+++ DashBoardMinimalAPI/StaticDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace DashBoardMinimalAPI
+{
+    public static class StaticDetails
+    {
+        static StaticDetails()
+        {
+            for (var i = 1; i<= 20; i++)
+            {
+                RoomsAvailability.Add(i, Renovation);
+            }
+        }
+        public const string Available = "available";
+        public const string Busy = "busy";
+        public const string Cleaning = "cleaning";
+        public const string Renovation = "renovation";
+
+        readonly static Dictionary<int, string> RoomsAvailability;
+      
+    }
+}
